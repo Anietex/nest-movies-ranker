0c91c84da3eb1882eef7af0c08eedfabfc7a6040
Aniefon Umanah <aniefon.umanah@gmail.com>
2023-03-19 14:50:59 +0100

    [IMP] - minor fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './modules/todo/todo.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthMiddleware } from './modules/auth/auth.middleware';
 import { RouteInfo } from '@nestjs/common/interfaces';
 import { MovieModule } from './modules/movie/movie.module';
-import { RankingModule } from './modules/ranking/ranking.module';
 
 @Module({
-  imports: [TodoModule, AuthModule, UserModule, MovieModule, RankingModule],
+  imports: [AuthModule, UserModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
@@ -19,7 +17,7 @@ export class AppModule {
     const excludedPaths: RouteInfo[] = [
       { path: '/auth/signin', method: RequestMethod.POST },
       { path: '/users/signup', method: RequestMethod.POST },
-      { path: 'api', method: RequestMethod.GET },
+      { path: 'api-docs', method: RequestMethod.GET },
     ];
     consumer
       .apply(AuthMiddleware)
